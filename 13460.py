ca2bf519ae17e36a54a63f6ae8d851cd6986df88
marmot8080 <marmot8080@gmail.com>
2024-06-07 14:00:12 +0900

    Shortened runtime

--- 13460.py
+++ 13460.py
@@ -1,173 +1,78 @@
-def tilt_right(Rx, Ry, Bx, By, count):
-    global min
+min = 11
+directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+def tilt_board(red_loc, blue_loc, direction, count):
+    global min, directions
+
     count += 1
 
     if count > 10:
         pass
     else:
-        rx = Rx
-        ry = Ry
-        bx = Bx
-        by = By
+        red = red_loc.copy()
+        blue = blue_loc.copy()
 
-        while(board[rx][ry + 1] != '#' and board[rx][ry + 1] != 'O'):
-            ry += 1
+        while(board[red[0]][red[1]] != '#' and board[red[0]][red[1]] != 'O'):
+            red[0] += direction[0]
+            red[1] += direction[1]
 
-        while(board[bx][by + 1] != '#' and board[bx][by + 1] != 'O'):
-            by += 1
+        while(board[blue[0]][blue[1]] != '#' and board[blue[0]][blue[1]] != 'O'):
+            blue[0] += direction[0]
+            blue[1] += direction[1]
         
-        if board[bx][by + 1] == 'O':
+        if board[blue[0]][blue[1]] == 'O':
             pass
-        elif board[rx][ry + 1] == 'O' and count < min:
+        elif board[red[0]][red[1]] == 'O' and count < min:
             min = count
-        elif rx == bx and ry == by:
-            if Ry > By:
-                by -= 1
-            else:
-                ry -= 1
+        elif red == blue:
+            red_tmp = list([red_loc[0] * direction[0], red_loc[1] * direction[1]])
+            blue_tmp = list([blue_loc[0] * direction[0], blue_loc[1] * direction[1]])
+            if red_tmp < blue_tmp:
+                red[0] -= direction[0]
+                red[1] -= direction[1]
+            elif red_tmp > blue_tmp:
+                blue[0] -= direction[0]
+                blue[1] -= direction[1]
             
-            tilt_right(rx, ry, bx, by, count)
-            tilt_backward(rx, ry, bx, by, count)
-            tilt_forward(rx, ry, bx, by, count)
-        else:
-            tilt_right(rx, ry, bx, by, count)
-            tilt_backward(rx, ry, bx, by, count)
-            tilt_forward(rx, ry, bx, by, count)
-
-def tilt_left(Rx, Ry, Bx, By, count):
-    global min
-    count += 1
-
-    if count > 10:
-        pass
-    else:
-        rx = Rx
-        ry = Ry
-        bx = Bx
-        by = By
-
-        while(board[rx][ry - 1] != '#' and board[rx][ry - 1] != 'O'):
-            ry -= 1
-
-        while(board[bx][by - 1] != '#' and board[bx][by - 1] != 'O'):
-            by -= 1
-        
-        if board[bx][by - 1] == 'O':
-            pass
-        elif board[rx][ry - 1] == 'O' and count < min:
-            min = count
-        elif rx == bx and ry == by:
-            if Ry > By:
-                ry += 1
-            else:
-                by += 1
+            red[0] -= direction[0]
+            red[1] -= direction[1]
+            blue[0] -= direction[0]
+            blue[1] -= direction[1]
             
-            tilt_left(rx, ry, bx, by, count)
-            tilt_backward(rx, ry, bx, by, count)
-            tilt_forward(rx, ry, bx, by, count)
+            opposite_direction = list([direction[0] * -1, direction[1] * -1])
+            for d in directions:
+                if d != direction and d != opposite_direction:
+                    tilt_board(red, blue, d, count)
         else:
-            tilt_left(rx, ry, bx, by, count)
-            tilt_backward(rx, ry, bx, by, count)
-            tilt_forward(rx, ry, bx, by, count)
+            red[0] -= direction[0]
+            red[1] -= direction[1]
+            blue[0] -= direction[0]
+            blue[1] -= direction[1]
 
-def tilt_backward(Rx, Ry, Bx, By, count):
-    global min
-    count += 1
+            opposite_direction = list([direction[0] * -1, direction[1] * -1])
+            for d in directions:
+                if d != direction and d != opposite_direction:
+                    tilt_board(red, blue, d, count)
 
-    if count > 10:
-        pass
-    else:
-        rx = Rx
-        ry = Ry
-        bx = Bx
-        by = By
-
-        while(board[rx + 1][ry] != '#' and board[rx + 1][ry] != 'O'):
-            rx += 1
-
-        while(board[bx + 1][by] != '#' and board[bx + 1][by] != 'O'):
-            bx += 1
-        
-        if board[bx + 1][by] == 'O':
-            pass
-        elif board[rx + 1][ry] == 'O' and count < min:
-            min = count
-        elif rx == bx and ry == by:
-            if Rx > Bx:
-                bx -= 1
-            else:
-                rx -= 1
-            
-            tilt_right(rx, ry, bx, by, count)
-            tilt_left(rx, ry, bx, by, count)
-            tilt_backward(rx, ry, bx, by, count)
-        else:
-            tilt_right(rx, ry, bx, by, count)
-            tilt_left(rx, ry, bx, by, count)
-            tilt_backward(rx, ry, bx, by, count)
-
-def tilt_forward(Rx, Ry, Bx, By, count):
-    global min
-    count += 1
-
-    if count > 10:
-        pass
-    else:
-        rx = Rx
-        ry = Ry
-        bx = Bx
-        by = By
-
-        while(board[rx - 1][ry] != '#' and board[rx - 1][ry] != 'O'):
-            rx -= 1
-
-        while(board[bx - 1][by] != '#' and board[bx - 1][by] != 'O'):
-            bx -= 1
-        
-        if board[bx - 1][by] == 'O':
-            pass
-        elif board[rx - 1][ry] == 'O' and count < min:
-            min = count
-        elif rx == bx and ry == by:
-            if Rx > Bx:
-                rx += 1
-            else:
-                bx += 1
-            
-            tilt_right(rx, ry, bx, by, count)
-            tilt_left(rx, ry, bx, by, count)
-            tilt_forward(rx, ry, bx, by, count)
-        else:
-            tilt_right(rx, ry, bx, by, count)
-            tilt_left(rx, ry, bx, by, count)
-            tilt_forward(rx, ry, bx, by, count)
-
-def get_least_movement(column, row):
-    global min
+def get_least_movement(row, column):
+    global directions
 
     for i in range(row):
         for j in range(column):
             if board[i][j] == 'R':
-                Rx = i
-                Ry = j
+                red_loc = [i, j]
             elif board[i][j] == 'B':
-                Bx = i
-                By = j
+                blue_loc = [i, j]
     
-    tilt_right(Rx, Ry, Bx, By, 0)
-    tilt_left(Rx, Ry, Bx, By, 0)
-    tilt_backward(Rx, Ry, Bx, By, 0)
-    tilt_forward(Rx, Ry, Bx, By, 0)
+    for d in directions:
+        tilt_board(red_loc, blue_loc, d, 0)
 
     if min > 10:
         return -1
     else:
         return min
 
-if __name__ == '__main__':
-    global board, min
-    min = 11
-    row, column = map(int, input().split())
-    board = [list(input()) for _ in range(row)]
+row, column = map(int, input().split())
+board = [list(input()) for _ in range(row)]
 
-    print(get_least_movement(column, row))
+print(get_least_movement(row, column))
