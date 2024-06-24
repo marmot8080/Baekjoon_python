397d9bb0351a27d6d6d6f7d98eedbde05111578d
marmot8080 <marmot8080@gmail.com>
2024-06-24 13:44:17 +0900

    Runtime error occurred

--- 12100.py
+++ 12100.py
@@ -10,22 +10,23 @@ def move_by_direction(count, board, direction):
     tmp_board = [row[:] for row in board]
 
     if direction[0] + direction[1] > 0:
-        # TODO: 공백(0) 제거
+        remove_blank_by_direction(tmp_board, direction)
         for i in range(n+1, 1, -1):
             for j in range(n+1, 1, -1):
                 if tmp_board[i][j] == tmp_board[i - direction[0]][j - direction[1]]:
                     tmp_board[i][j] *= 2
                     tmp_board[i - direction[0]][j - direction[1]] = 0
-        # TODO: 공백(0) 제거
+        remove_blank_by_direction(tmp_board, direction)
     else:
-        # TODO: 공백(0) 제거
+        remove_blank_by_direction(tmp_board, direction)
         for i in range(1, n+1):
             for j in range(1, n+1):
-                if tmp_board[i][j] == tmp_board[i - direction[0]][j - direction[1]]:
-                    tmp_board[i][j] *= 2
-                    tmp_board[i - direction[0]][j - direction[1]] = 0
-        # TODO: 공백(0) 제거
-
+                if tmp_board[i][j] == 0:
+                    for k in range(1, abs(i*direction[0] + j*direction[1]) - 1):
+                        if tmp_board[i - k*direction[0]][j - k*direction[1]] != 0:
+                            tmp_board[i][j] = tmp_board[i - k*direction[0]][j - k*direction[1]]
+                            tmp_board[i - k*direction[0]][j - k*direction[1]] = 0
+        remove_blank_by_direction(tmp_board, direction)
     if count == 4:
         for i in range(1, n+1):
             for j in range(1, n+1):
@@ -37,21 +38,40 @@ def move_by_direction(count, board, direction):
         move_by_direction(count + 1, tmp_board, up)
         move_by_direction(count + 1, tmp_board, down)
 
-def get_available_biggest_block():
-    move_by_direction(0, main_board, left)
-    move_by_direction(0, main_board, right)
-    move_by_direction(0, main_board, up)
-    move_by_direction(0, main_board, down)
+def remove_blank_by_direction(board, direction):
+    if direction[0] + direction[1] > 0:
+        for i in range(n+1, 1, -1):
+            for j in range(n+1, 1, -1):
+                if board[i][j] == 0:
+                    for k in range(1, abs(i*direction[0] + j*direction[1]) - 1):
+                        if board[i - k*direction[0]][j - k*direction[1]] != 0:
+                            board[i][j] = board[i - k*direction[0]][j - k*direction[1]]
+                            board[i - k*direction[0]][j - k*direction[1]] = 0
+    else:
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if board[i][j] == 0:
+                    for k in range(1, abs(i*direction[0] + j*direction[1]) - 1):
+                        if board[i - k*direction[0]][j - k*direction[1]] != 0:
+                            board[i][j] = board[i - k*direction[0]][j - k*direction[1]]
+                            board[i - k*direction[0]][j - k*direction[1]] = 0
 
-n = int(input())
+def get_available_biggest_block(board):
+    move_by_direction(0, board, left)
+    move_by_direction(0, board, right)
+    move_by_direction(0, board, up)
+    move_by_direction(0, board, down)
 
-main_board = [[-1 for _ in range(n+2)] for _ in range(n+2)]
+if __name__ == '__main__':
+    n = int(input())
 
-for i in range(1, n+1):
-    row = [m for m in map(int, input().split())]
-    for j in range(1, n+1):
-        main_board[i][j] = row[j-1]
+    board = [[-1 for _ in range(n+2)] for _ in range(n+2)]
 
-get_available_biggest_block()
+    for i in range(1, n+1):
+        row = [m for m in map(int, input().split())]
+        for j in range(1, n+1):
+            board[i][j] = row[j-1]
 
-print(max)
+    get_available_biggest_block(board)
+
+    print(max)
