c093e30b56c88ae8610c4c99704138a56e26a5ec
marmot8080 <marmot8080@gmail.com>
2024-07-09 15:18:12 +0900

    solved 14501

--- 14501.py
+++ 14501.py
@@ -1,13 +1,28 @@
 import sys
 
-def get_max_profit(count, time_taken, cost):
-    pass
+def max_profit(n, sum):
+    global count, max_sum
+
+    if n >= count:
+        if sum > max_sum:
+            max_sum = sum
+        return
+
+    max_profit(n + 1, sum)
+    if n + time_taken[n] <= count:
+        max_profit(n + time_taken[n], sum + cost[n])
 
 if __name__ == '__main__':
+    global count, max_sum
+    
+    max_sum = 0
     count = int(input())
     time_taken = [0 for _ in range(count)]
     cost = [0 for _ in range(count)]
 
     for i in range(count):
-        time_taken[i], cost[i] = map(int, sys.stdin.readlie().split())
+        time_taken[i], cost[i] = map(int, sys.stdin.readline().split())
     
+    max_profit(0, 0)
+
+    print(max_sum)
