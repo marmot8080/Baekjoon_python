00dd45f82b1508e886da9f2d99ea77747a0c3be5
marmot8080 <marmot8080@gmail.com>
2024-08-26 15:59:08 +0900

    solved 18870

--- 18870.py
+++ 18870.py
@@ -1,15 +1,10 @@
-import sys
+from sys import stdin
 
 if __name__ == '__main__':
     n = int(input())
-    x = [num for num in map(int, sys.stdin.readline().split())]
-    sorted_x = sorted(x)
-    result = []
+    x = [num for num in map(int, stdin.readline().split())]
+    sorted_x = sorted(set(x))
+    dic = {sorted_x[i]: i for i in range(len(sorted_x))}
 
-    a = 0
-    while a < n:
-        result.append(sorted_x[a])
-        a += sorted_x.count(sorted_x[a])
-    
     for i in x:
-        print(result.index(i), end=' ')
+        print(dic[i], end=' ')
